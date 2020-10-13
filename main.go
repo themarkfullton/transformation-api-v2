76258b891cd4f5f0f71541160958c0a2bce21f40
Mark <the.mark.fullton@gmail.com>
2020-10-12 17:04:34 -0700

    Added get all clinics

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import(
+	"context"
+	"encoding/json"
 	"log"
 	"net/http"
 	"os"
@@ -12,6 +14,41 @@ import(
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
+
+func getAllClinics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-Type", "application/json")
+
+	var clinics []models.Clinic
+
+	collection := helper.ConnectDB("clinics")
+
+	cursor, err := collection.Find(context.TODO(), bson.M{})
+
+	if err != nil {
+		helper.GetError(err, w)
+		return
+	}
+
+	defer cursor.Close(context.TODO())
+
+	for cursor.Next(context.TODO()) {
+		var clinic models.Clinic
+		err := cursor.Decode(&clinic)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		clinics = append(clinics, clinic)
+	}
+
+	if err := cursor.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	json.NewEncoder(w).Encode(clinics)
+}
 
 func main(){
 	r := mux.NewRouter()
